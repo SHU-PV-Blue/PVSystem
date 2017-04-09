aaf49a86d8a49b85bd8e0a6687238d5c4d224fdb
chengsluo <chengsluo@gmail.com>
2017-04-09 08:48:06 +0800

    Chengsluo 的修改

--- src/main/java/com/shupv/entity/HighPressureDevice.java
+++ src/main/java/com/shupv/entity/HighPressureDevice.java
@@ -9,4 +9,28 @@ public class HighPressureDevice {
     private int deviceSize;//设备类型
     private int subHighPressure;//子类的设备编号
     public HighPressureDevice(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDeviceSize() {
+        return deviceSize;
+    }
+
+    public void setDeviceSize(int deviceSize) {
+        this.deviceSize = deviceSize;
+    }
+
+    public int getSubHighPressure() {
+        return subHighPressure;
+    }
+
+    public void setSubHighPressure(int subHighPressure) {
+        this.subHighPressure = subHighPressure;
+    }
 }
